53f8ac2406f2ea5c0d591032943dc755b7127562
LOGESH-B <logeshbommannan@gmail.com>
2024-01-21 17:51:40 +0530

    bugfix/3

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
+  app.use(express.static(path.join(__dirname, 'public')));
 
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
